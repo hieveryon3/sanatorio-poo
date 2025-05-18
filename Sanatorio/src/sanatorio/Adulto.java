208d4c150e17b2d52dca503e58d917a7824b3103
Emiliano Ezequiel <ojedaezequiel.dev@gmail.com>
2025-05-18 19:59:03 -0300

    Implementamos la interfaz dentro de la clase Adulto e Internado

--- Sanatorio/src/sanatorio/Adulto.java
+++ Sanatorio/src/sanatorio/Adulto.java
@@ -1,8 +1,10 @@
 
 package sanatorio;
 
+import sanatorio.interfaces.Administracion; // importamos la interfaz administracion
 
-public class Adulto extends Paciente{
+
+public class Adulto extends Paciente implements Administracion{ //utilizamos implements y el nombre de la interfaz para implementar en la clase esos metodos
     protected String presionArterial;
     protected double altura;
     protected double peso;
@@ -12,5 +14,19 @@ public class Adulto extends Paciente{
         this.presionArterial = presionArterial;
         this.altura = altura;
         this.peso = peso;
+    }
+    @Override
+    public String getCoberturaOS(){
+        return obraSocial;
+    }
+    
+    @Override
+    public double getDescuento(){
+        return 0.05;
+    }
+    
+    @Override
+    public String getVademecum(){
+        return "Paracetamol, Ibuprofeno";
     }
 }
